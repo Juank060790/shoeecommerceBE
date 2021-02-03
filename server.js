611d65a733b457ce3eea6146abf11569a9d9ab6c
JuanCa <juank060790@gmail.com>
2021-02-03 15:37:34 +0700

    login fixed

--- server.js
+++ server.js
@@ -7,22 +7,21 @@ import orderRouter from "./routers/orderRouter.js";
 import cors from "cors";
 
 dotenv.config();
-// const cors = require("cors");
 const mongoURI = process.env.MONGODB;
-
+const fronEndHost = process.env.FRONTENDHOST;
 var app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5000/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", fronEndHost); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(mongoURI, {
--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = "somethingsecret";
 
 export const generateToken = (user) => {
   return jwt.sign(
